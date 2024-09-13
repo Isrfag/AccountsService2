c3dde6694bbbc0bbf6a94f6092e948204b2c3965
Heri Amezcua <amezcua.cloud@gmail.com>
2024-09-13 15:23:38 +0200

    update

--- src/main/java/com/microcompany/accountsservice/config/GlobalExceptionController.java
+++ src/main/java/com/microcompany/accountsservice/config/GlobalExceptionController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.validation.ConstraintViolationException;
@@ -19,7 +20,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionController {
 
     @ExceptionHandler(GlobalException.class)
